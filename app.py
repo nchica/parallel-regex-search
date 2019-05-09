3f8ec19740685fc57a37efacccb583ddc7c2dda4
Nathan Chica <n_chica@u.pacific.edu>
2019-05-08 17:17:54 -0700

    csv now prints results

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import re
 import sys
@@ -7,6 +8,7 @@ from glob import glob
 
 DIRECTORY_TO_CHECK = "."
 EXTENSIONS = ['*.txt', '*.log']
+HEADERS = ["Filepath", "Line Number", "Line"]
 
 
 def prompt_regex_string():
@@ -29,13 +31,21 @@ def check_file_for_regex_str(filepath, regex_str):
     results = []
     with open(filepath) as file_to_check:
         line_num = 1
-        lines_in_file = [line for line in file_to_check]
+        # lines_in_file = [line for line in file_to_check]
         for line in file_to_check:
             if string_contains_regex_str(line, regex_str):
                 formatted_line = line.rstrip('\n')
-                results.append(f"{filepath},{line_num},{formatted_line}")
+                results.append([filepath, line_num, formatted_line])
             line_num += 1
     return results
+
+
+def print_results_to_csv(results):
+    print(results)
+    with open("results.csv", mode="w", newline='') as results_file:
+        results_writer = csv.writer(results_file)
+        results_writer.writerow(HEADERS)
+        results_writer.writerows(results)
 
 
 def main():
@@ -44,8 +54,11 @@ def main():
 
     filepaths_to_check = get_filepaths_to_check()
 
+    results = []
     for filepath in filepaths_to_check:
-        check_file_for_regex_str(filepath, regex_str)
+        results.extend(check_file_for_regex_str(filepath, regex_str))
+
+    print_results_to_csv(results)
 
 
 if __name__ == '__main__':
